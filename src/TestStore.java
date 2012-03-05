7cce6efbf7f326ffe0a420d6a7cd7f5a63861e98
Niko Schwarz <niko.schwarz@gmail.com>
2012-03-05 18:23:04 +0100

    More useful TestStore

--- src/TestStore.java
+++ src/TestStore.java
@@ -11,6 +11,17 @@ import org.apache.hadoop.hbase.util.Bytes;
 public class TestStore {
 	public static void main(String[] args) throws IOException {
 		String [] pages = {"/", "/a.html", "/b.html", "/c.html"};
+        byte[] b = new byte[2048];
+        for(int i=0;i<2048;i+=8) {
+        		b[i] =  (byte)  0xc;
+        		b[i+1] = (byte) 0xa;
+        		b[i+2] = (byte) 0xf;
+        		b[i+3] = (byte) 0xe;
+        		b[i+4] = (byte) 0xb;
+        		b[i+5] = (byte) 0xa;
+        		b[i+6] = (byte) 0xb;
+        		b[i+7] = (byte) 0xe;
+        }
         
         Configuration hbaseConfig = HBaseConfiguration.create();
         HTable htable = new HTable(hbaseConfig, "t1");
@@ -27,7 +38,7 @@ public class TestStore {
             byte [] rowkey = Bytes.add(Bytes.toBytes(userID), Bytes.toBytes(i));
             String randomPage = pages[rand.nextInt(pages.length)];
             Put put = new Put(rowkey);
-            put.add(Bytes.toBytes("f1"), Bytes.toBytes("c1"), 0l ,Bytes.toBytes(randomPage));
+            put.add(Bytes.toBytes("f1"), Bytes.toBytes("c1"), 0l , b);
             htable.put(put);
         }
         htable.flushCommits();
