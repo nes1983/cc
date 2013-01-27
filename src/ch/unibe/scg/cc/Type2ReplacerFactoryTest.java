98b7d3f56762d67a5671055c46341a4f4bd23624
Niko Schwarz <niko.schwarz@gmail.com>
2013-01-27 18:03:36 +0100

    Make test green.
    The code was right, the test was wrong.

--- src/ch/unibe/scg/cc/Type2ReplacerFactoryTest.java
+++ src/ch/unibe/scg/cc/Type2ReplacerFactoryTest.java
@@ -1,8 +1,7 @@
 package ch.unibe.scg.cc;
 
-import static org.hamcrest.Matchers.arrayWithSize;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,25 +13,24 @@ import ch.unibe.scg.cc.regex.Replace;
 @RunWith(JExample.class)
 public class Type2ReplacerFactoryTest {
 
+	String sampleString() {
+		return "\t\tfish.stink.Rod.doIt(new int[] { 1, 2 ,3 });\n" + "	}\n";
+	}
+
 	@Test
 	public PhaseFrontend testFactory() {
-		Type2ReplacerFactory factory = new Type2ReplacerFactory();
-		Replace[] replaces = factory.get();
+		final Type2ReplacerFactory factory = new Type2ReplacerFactory();
+		final Replace[] replaces = factory.get();
 		assertThat(replaces, is(arrayWithSize(3)));
-		Normalizer normalizer = new Normalizer(replaces);
+		final Normalizer normalizer = new Normalizer(replaces);
 		return normalizer;
 	}
 
 	@Given("testFactory")
 	public PhaseFrontend testNormalize(PhaseFrontend phase) {
-		StringBuilder sb = new StringBuilder(sampleString());
+		final StringBuilder sb = new StringBuilder(sampleString());
 		phase.normalize(sb);
-		assertThat(sb.toString(), is("\t\tt. t.t. t(t t[] { 1, 1 ,1 });\n\t}\n"));
+		assertThat(sb.toString(), is("\t\tt. t. t. t(t t[] { 1, 1 ,1 });\n\t}\n"));
 		return phase;
 	}
-
-	String sampleString() {
-		return "\t\tfish.stink.Rod.doIt(new int[] { 1, 2 ,3 });\n" + "	}\n";
-	}
-
 }
