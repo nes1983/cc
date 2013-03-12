8df3ea8f22a32d6f9c75b7fe421ba3409c35a57b
Simon Vogt <vsiandroid@gmail.com>
2013-03-12 23:03:22 +0100

    Additional JavaTokenizerTest
    The current regex doesn't work on more complex classes. Needs fixing!

--- src/ch/unibe/scg/cc/javaFrontend/JavaTokenizerTest.java
+++ src/ch/unibe/scg/cc/javaFrontend/JavaTokenizerTest.java
@@ -80,4 +80,92 @@ public class JavaTokenizerTest {
 				+ "	}\n" + "\t\t\tprotected static void doIt() {\n" + "\t\tif(System.timeInMillis() > 10000)\n"
 				+ "\t\t\tout.println(1337);\n" + "\t\tmain(null);\n" + "\t}\n" + "}\n";
 	}
+
+	@Test
+	public void testBiggerSampleClass() {
+		JavaTokenizer tokenizer = new JavaTokenizer();
+		FunctionFactory functionFactory = Mockito.mock(FunctionFactory.class);
+		tokenizer.functionFactory = functionFactory;
+
+		List<Function> list = tokenizer.tokenize(biggerSampleClass(), null);
+
+		assertThat(list.size(), is(6));
+		verify(functionFactory).makeFunction(any(Hasher.class), eq(0),
+				eq("package com.stradivari.util;\n" + "\n" + "import java.math.RoundingMode;\n" + "\n"));
+		verify(functionFactory)
+				.makeFunction(
+						any(Hasher.class),
+						eq(4),
+						eq("public class Math {\n"
+								+ "\tstatic final int[] POWERS_OF_10 = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };\n"
+								+ "\tstatic final int[] HALF_POWERS_OF_10 = { 3, 31, 316, 3162, 31622, 316227, 3162277, 31622776, 316227766,\n"
+								+ "\t\t\tInteger.MAX_VALUE };\n"
+								+ "\tstatic final byte[] MAX_LOG_10_FOR_LEADING_ZEROS = { 9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0 };\n"
+								+ "\n"));
+		verify(functionFactory).makeFunction(
+				any(Hasher.class),
+				eq(10),
+				eq("\tpublic static int log10(int x, RoundingMode mode) {\n" + "\t\tcheckPositive(\"x\", x);\n"
+						+ "\t\tint logFloor = log10Floor(x);\n" + "\t\tint floorPow = POWERS_OF_10[logFloor];\n"
+						+ "\t\tint result = -1;\n" + "\t\tswitch (mode) {\n" + "\t\tcase UNNECESSARY:\n"
+						+ "\t\t\tcheckRoundingUnnecessary(x == floorPow);\n" + "\t\t\t// fall through\n"
+						+ "\t\tcase DOWN:\n" + "\t\t\tresult = logFloor;\n" + "\t\tcase CEILING:\n" + "\t\tcase UP:\n"
+						+ "\t\t\tresult = (x == floorPow) ? logFloor : logFloor - 1;\n" + "\t\tcase HALF_DOWN:\n"
+						+ "\t\tcase HALF_UP:\n" + "\t\tcase HALF_EVEN:\n"
+						+ "\t\t\t// sqrt(10) is irrational, so log10(x) - logFloor is never exactly\n"
+						+ "\t\t\t// 0.5\n"
+						+ "\t\t\tresult = (x <= HALF_POWERS_OF_10[logFloor]) ? logFloor : logFloor - 1;\n" + "\t\t}\n"
+						+ "\t\treturn result;\n" + "\t}\n" + "\n"));
+		verify(functionFactory).makeFunction(
+				any(Hasher.class),
+				eq(34),
+				eq("\tprivate static int log10Floor(int x) {\n"
+						+ "\t\tint y = MAX_LOG_10_FOR_LEADING_ZEROS[Integer.numberOfLeadingZeros(x)];\n"
+						+ "\t\tint sgn = (x - POWERS_OF_10[y]) >>> (Integer.SIZE - 1);\n" + "\t\treturn y - sgn;\n"
+						+ "\t}\n" + "\n"));
+		verify(functionFactory)
+				.makeFunction(
+						any(Hasher.class),
+						eq(40),
+						eq("\tstatic void checkRoundingUnnecessary(boolean condition) {\n"
+								+ "\t\tif (!condition) {\n"
+								+ "\t\t\tthrow new ArithmeticException(\"mode was UNNECESSARY, but rounding was necessary\");\n"
+								+ "\t\t}\n" + "\t}\n" + "\n"));
+		verify(functionFactory).makeFunction(
+				any(Hasher.class),
+				eq(46),
+				eq("\tstatic int checkPositive(String role, int x) {\n" + "\t\tif (x <= 0) {\n"
+						+ "\t\t\tthrow new IllegalArgumentException(role + \" (\" + x + \") must be > 0\");\n"
+						+ "\t\t}\n" + "\t\treturn x;\n" + "\t}\n" + "}\n"));
+	}
+
+	String biggerSampleClass() {
+		return "package com.stradivari.util;\n"
+				+ "\n"
+				+ "import java.math.RoundingMode;\n"
+				+ "\n"
+				+ "public class Math {\n"
+				+ "\tstatic final int[] POWERS_OF_10 = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };\n"
+				+ "\tstatic final int[] HALF_POWERS_OF_10 = { 3, 31, 316, 3162, 31622, 316227, 3162277, 31622776, 316227766,\n"
+				+ "\t\t\tInteger.MAX_VALUE };\n"
+				+ "\tstatic final byte[] MAX_LOG_10_FOR_LEADING_ZEROS = { 9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0 };\n"
+				+ "\n" + "\tpublic static int log10(int x, RoundingMode mode) {\n" + "\t\tcheckPositive(\"x\", x);\n"
+				+ "\t\tint logFloor = log10Floor(x);\n" + "\t\tint floorPow = POWERS_OF_10[logFloor];\n"
+				+ "\t\tint result = -1;\n" + "\t\tswitch (mode) {\n" + "\t\tcase UNNECESSARY:\n"
+				+ "\t\t\tcheckRoundingUnnecessary(x == floorPow);\n" + "\t\t\t// fall through\n" + "\t\tcase DOWN:\n"
+				+ "\t\t\tresult = logFloor;\n" + "\t\tcase CEILING:\n" + "\t\tcase UP:\n"
+				+ "\t\t\tresult = (x == floorPow) ? logFloor : logFloor - 1;\n" + "\t\tcase HALF_DOWN:\n"
+				+ "\t\tcase HALF_UP:\n" + "\t\tcase HALF_EVEN:\n"
+				+ "\t\t\t// sqrt(10) is irrational, so log10(x) - logFloor is never exactly\n" + "\t\t\t// 0.5\n"
+				+ "\t\t\tresult = (x <= HALF_POWERS_OF_10[logFloor]) ? logFloor : logFloor - 1;\n" + "\t\t}\n"
+				+ "\t\treturn result;\n" + "\t}\n" + "\n" + "\tprivate static int log10Floor(int x) {\n"
+				+ "\t\tint y = MAX_LOG_10_FOR_LEADING_ZEROS[Integer.numberOfLeadingZeros(x)];\n"
+				+ "\t\tint sgn = (x - POWERS_OF_10[y]) >>> (Integer.SIZE - 1);\n" + "\t\treturn y - sgn;\n" + "\t}\n"
+				+ "\n" + "\tstatic void checkRoundingUnnecessary(boolean condition) {\n" + "\t\tif (!condition) {\n"
+				+ "\t\t\tthrow new ArithmeticException(\"mode was UNNECESSARY, but rounding was necessary\");\n"
+				+ "\t\t}\n" + "\t}\n" + "\n" + "\tstatic int checkPositive(String role, int x) {\n"
+				+ "\t\tif (x <= 0) {\n"
+				+ "\t\t\tthrow new IllegalArgumentException(role + \" (\" + x + \") must be > 0\");\n" + "\t\t}\n"
+				+ "\t\treturn x;\n" + "\t}\n" + "}\n";
+	}
 }
