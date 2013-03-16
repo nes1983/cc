085ea30729f8c6d9f8c412c0f0de093b9d9a92ca
Niko Schwarz <niko.schwarz@gmail.com>
2013-03-16 04:58:29 +0100

    Fixed SnippetMatchComparator.
    The previous comparator did not output valid
    output for a comparator, there led to random sorting.

--- src/ch/unibe/scg/cc/SnippetSimilarTest.java
+++ src/ch/unibe/scg/cc/SnippetSimilarTest.java
@@ -1,4 +1,5 @@
 package ch.unibe.scg.cc;
+
 import static ch.unibe.scg.cc.RegisterClonesBackend.MINIMUM_LINES;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -27,6 +28,7 @@ import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.protobuf.ByteString;
 
+// @formatter:off
 /**
  * Test {@link CloneExpander}.
  * @author Niko Schwarz
@@ -42,7 +44,7 @@ public final class SnippetSimilarTest {
 							o2.getThatSnippetLocation().getFunction().asReadOnlyByteBuffer())
 					.compare(
 							o1.getThisSnippetLocation().getPosition(),
-							o2.getThatSnippetLocation().getPosition())
+							o2.getThisSnippetLocation().getPosition())
 					.result();
 		}
 	}
@@ -107,23 +109,23 @@ public final class SnippetSimilarTest {
 
 		assertThat(matches.size(), is(3));
 		assertThat(printString(matches.get(0)),
-				is("(1-2|5$e556@3, 1-2|14$6043@13, 1-2|16$24a1@15, 1-3|16$24a1@12, 1-3|14$6043@10, )"));
-		assertThat(printString(matches.get(1)), is("(2-3|0$832c@0, 2-3|10$a370@7, 2-3|7$650e@4, " +
-				"2-3|12$cbd8@9, 2-3|11$270e@8, 2-3|9$2f8d@6, 2-3|8$68b6@5, 2-3|17$8cb0@14, " +
-				"2-3|16$0b4c@13, 2-3|15$24a1@12, 2-3|14$5b92@11, 2-3|13$6043@10, )"));
+				is("(1-2|5$e556@3, 1-2|14$6043@13, 1-2|16$24a1@15, 1-3|14$6043@10, 1-3|16$24a1@12, )"));
+		assertThat(printString(matches.get(1)), is("(2-3|0$832c@0, 2-3|7$650e@4, 2-3|8$68b6@5, " +
+				"2-3|9$2f8d@6, 2-3|10$a370@7, 2-3|11$270e@8, 2-3|12$cbd8@9, 2-3|13$6043@10, 2-3|14$5b92@11," +
+				" 2-3|15$24a1@12, 2-3|16$0b4c@13, 2-3|17$8cb0@14, )"));
 		assertThat(printString(matches.get(2)), is("()"));
 
 		// Extract matches
 		Collection<Clone> builtClones = new CloneExpander().expandClones(matches.get(0));
 		assertThat(builtClones.toString(),
-				is("[this_function: \"\\001\"\nthat_function: \"\\002\"\n"
-				+ "this_from_position: 5\nthat_from_position: 3\nthis_length: 16\nthat_length: 17\n"
-				+ ", this_function: \"\\001\"\nthat_function: \"\\003\"\n"
-				+ "this_from_position: 16\nthat_from_position: 10\nthis_length: 3\nthat_length: 5\n]"));
+				is("[this_function: \"\\001\"\nthat_function: \"\\002\"\n" +
+						"this_from_position: 5\nthat_from_position: 3\nthis_length: 16\nthat_length: 17\n," +
+						" this_function: \"\\001\"\nthat_function: \"\\003\"\n" +
+						"this_from_position: 14\nthat_from_position: 10\nthis_length: 7\nthat_length: 7\n]"));
 		builtClones = new CloneExpander().expandClones(matches.get(1));
 		assertThat(builtClones.toString(),
 				is("[this_function: \"\\002\"\nthat_function: \"\\003\"\n"
-				+ "this_from_position: 0\nthat_from_position: 0\nthis_length: 18\nthat_length: 19\n]"));
+				+ "this_from_position: 0\nthat_from_position: 0\nthis_length: 22\nthat_length: 19\n]"));
 		builtClones = new CloneExpander().expandClones(matches.get(2));
 		assertThat(builtClones.toString(), is("[]"));
 	}
@@ -159,7 +161,8 @@ public final class SnippetSimilarTest {
 				+ "			// fall through\n"
 				+ "		case DOWN:\n"
 				+ "			result = logFloor;\n"
-				+ "		case CEILING:\n" + "		case UP:\n"
+				+ "		case CEILING:\n" 
+				+ "		case UP:\n"
 				+ "			result = (x == floorPow) ? logFloor : logFloor - 1;\n"
 				+ "		case HALF_DOWN:\n"
 				+ "		case HALF_UP:\n"
