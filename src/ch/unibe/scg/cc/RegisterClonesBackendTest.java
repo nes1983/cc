7fe2a9c6688753cd37d4863f2a9da5bf4e57a60f
Simon Vogt <vsiandroid@gmail.com>
2013-03-18 01:11:18 +0100

    RegisterClonesBackendTest readjusted
    needed readjustemend because we now work with clones of 5 lines length.
    last line in a StringOfLines object doesn't get counted in (because it's mostly just a new line character),
    therefore the invocation count of the registerConsecutiveLinesOfCode decreases by 1.

--- src/ch/unibe/scg/cc/RegisterClonesBackendTest.java
+++ src/ch/unibe/scg/cc/RegisterClonesBackendTest.java
@@ -4,7 +4,6 @@ import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -31,9 +30,9 @@ public class RegisterClonesBackendTest {
 	final HTable htable = mock(HTable.class);
 	Function function;
 	final StringOfLinesFactory stringOfLinesFactory = new StringOfLinesFactory();
-	final StringOfLines sampleLines = stringOfLinesFactory.make("a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\n");
+	final StringOfLines sampleLines = stringOfLinesFactory.make("a\nb\nc\nd\ne\nf\n");
+	final StringOfLines aThruF = stringOfLinesFactory.make("a\nb\nc\nd\ne\nf\n");
 	final StringOfLines aThruK = stringOfLinesFactory.make("a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\n");
-	final StringOfLines aThruP = stringOfLinesFactory.make("a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm\nn\no\np\n");
 
 	@Test
 	public RegisterClonesBackend testOneRegister() {
@@ -46,13 +45,12 @@ public class RegisterClonesBackendTest {
 
 		project = mock(RealProject.class);
 		function = mock(Function.class);
-		stub(function.getBaseLine()).toReturn(3);
 
 		rcb.registerConsecutiveLinesOfCode(sampleLines, function, Main.TYPE_3_CLONE);
 
 		verify(cr).register(
-				eq(new byte[] { -22, -6, -54, -20, 74, -100, 75, 44, -1, -114, 117, 100, 14, 91, -69, -66, 115, -57,
-						54, 80 }), (String) anyObject(), eq(function), eq(0), eq(10), eq(Main.TYPE_3_CLONE));
+				eq(new byte[] { 69, 10, -93, -20, 53, -4, -66, -128, 103, -28, 44, 42, 38, -9, 20, -75, -38, 89, -71,
+						70 }), (String) anyObject(), eq(function), eq(0), eq(5), eq(Main.TYPE_3_CLONE));
 
 		Mockito.reset(cr); // JExample doesn't support @After
 		return rcb;
@@ -62,14 +60,10 @@ public class RegisterClonesBackendTest {
 	public RegisterClonesBackend testMoreRegisters(RegisterClonesBackend rcb) {
 		CloneRegistry cr = mock(CloneRegistry.class);
 		rcb.registry = cr;
-		rcb.registerConsecutiveLinesOfCode(aThruK, function, Main.TYPE_3_CLONE);
+		rcb.registerConsecutiveLinesOfCode(aThruF, function, Main.TYPE_3_CLONE);
 		verify(cr).register(
-				eq(new byte[] { -22, -6, -54, -20, 74, -100, 75, 44, -1, -114, 117, 100, 14, 91, -69, -66, 115, -57,
-						54, 80 }), (String) anyObject(), eq(function), eq(0), eq(10), eq(Main.TYPE_3_CLONE));
-		verify(cr)
-				.register(
-						eq(new byte[] { -70, 95, 22, -52, 75, -75, 56, 22, 37, -100, 43, 120, 70, 2, 77, 69, -58, -23,
-								20, 18 }), (String) anyObject(), eq(function), eq(1), eq(10), eq(Main.TYPE_3_CLONE));
+				eq(new byte[] { 69, 10, -93, -20, 53, -4, -66, -128, 103, -28, 44, 42, 38, -9, 20, -75, -38, 89, -71,
+						70 }), (String) anyObject(), eq(function), eq(0), eq(5), eq(Main.TYPE_3_CLONE));
 		Mockito.reset(cr); // JExample doesn't support @After
 		return rcb;
 	}
@@ -78,8 +72,8 @@ public class RegisterClonesBackendTest {
 	public RegisterClonesBackend testLotsOfRegisters(RegisterClonesBackend rcb) {
 		CloneRegistry cr = mock(CloneRegistry.class);
 		rcb.registry = cr;
-		rcb.registerConsecutiveLinesOfCode(aThruP, function, Main.TYPE_2_CLONE);
-		verify(cr, times(7)).register(((byte[]) anyObject()), (String) anyObject(), eq(function), anyInt(), anyInt(),
+		rcb.registerConsecutiveLinesOfCode(aThruK, function, Main.TYPE_2_CLONE);
+		verify(cr, times(6)).register(((byte[]) anyObject()), (String) anyObject(), eq(function), anyInt(), anyInt(),
 				eq(Main.TYPE_2_CLONE));
 		Mockito.reset(cr); // JExample doesn't support @After
 
