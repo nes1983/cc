82670d87da00c273b40c7873ed40c5ff560581b6
Simon Vogt <vsiandroid@gmail.com>
2013-03-19 02:18:37 +0100

    HBaseTest tests read/write performance
    if test fails, first delete the testtable on next launch.
    tests simple r/w operations on an HTable.

--- src/ch/unibe/scg/cc/mappers/HBaseTest.java
+++ src/ch/unibe/scg/cc/mappers/HBaseTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
@@ -15,6 +16,7 @@ import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -23,6 +25,7 @@ import ch.unibe.scg.cc.activerecord.ConfigurationProvider;
 import ch.unibe.scg.cc.modules.CCModule;
 import ch.unibe.scg.cc.modules.JavaModule;
 
+import com.google.common.base.Stopwatch;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -42,6 +45,12 @@ public class HBaseTest {
 		HTableDescriptor td = new HTableDescriptor(TEST_TABLE_NAME);
 		HColumnDescriptor family = new HColumnDescriptor(GuiceResource.FAMILY);
 		td.addFamily(family);
+		if (admin.isTableAvailable(TEST_TABLE_NAME)) {
+			if (admin.isTableEnabled(TEST_TABLE_NAME)) {
+				admin.disableTable(TEST_TABLE_NAME);
+			}
+			admin.deleteTable(TEST_TABLE_NAME);
+		}
 		admin.createTable(td);
 		admin.close();
 	}
@@ -58,6 +67,43 @@ public class HBaseTest {
 		testTable.flushCommits();
 		assertFalse(testTable.get(get).isEmpty());
 		testTable.close();
+	}
+
+	@Test
+	public void checkTimes() throws IOException {
+		HTable testTable = new HTable(conf, TEST_TABLE_NAME);
+		// settings adapted from HTableProvider
+		testTable.setAutoFlush(false);
+		testTable.setWriteBufferSize(1024 * 1024 * 12);
+		testTable.getTableDescriptor().setDeferredLogFlush(true);
+
+		final byte[] key = new byte[] { 123 };
+		final int rounds = 500;
+		final long time2Read, time2Write;
+
+		Stopwatch stopwatch = new Stopwatch().start();
+		for (int i = 0; i < rounds; i++) {
+			Put put = new Put(key);
+			put.setWriteToWAL(false);
+			put.add(GuiceResource.FAMILY, key, 0l, key);
+			testTable.put(put);
+		}
+		testTable.flushCommits();
+		stopwatch.stop();
+		time2Write = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+
+		// not using caching on purpose, simulates random read on disk
+		stopwatch.reset().start();
+		for (int i = 0; i < rounds; i++) {
+			Get get = new Get(key);
+			if (testTable.get(get).isEmpty()) {
+				Assert.fail("Key should exist here!");
+			}
+		}
+		stopwatch.stop();
+		time2Read = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+		testTable.close();
+		assertTrue(time2Write < time2Read);
 	}
 
 	@After
