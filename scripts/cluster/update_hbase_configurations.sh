f6bf7e96307b9e8863285a2064573ac26638651d
Simon Vogt <vsiandroid@gmail.com>
2013-03-24 23:36:25 +0100

    Cluster update scripts
    renamed hadoop update script
    added hbase update script

---
+++ scripts/cluster/update_hbase_configurations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# call this script to update the hbase configuration on nodes in the cluster
+# just add the hostnames as arguments, and the hbase-site.xml and hbase-env.sh will be stored under /tmp/ on the corresponding host
+
+# ssh-user: store the username in a separate file "sshuser"
+USER=$(cat sshuser)
+
+for w in $@; do
+	rsync -av "../../hbase-conf/${w}/hbase-site.xml" "$USER@${w}:/tmp/"
+	rsync -av "../../hbase-conf/hbase-env.sh" "$USER@${w}:/tmp/"
+done
+
+$(csshX --login=$USER $@)
