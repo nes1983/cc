42bd2838bbf1888e872f649d9d78d58010342025
Simon Vogt <vsiandroid@gmail.com>
2013-03-28 12:28:51 +0100

    GitCloner now handles Subversion repos too
    added super simple support for Subversion repositories

--- scripts/ohloh/GitCloner.rb
+++ scripts/ohloh/GitCloner.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/env jruby
 
 # single threaded solution
+require 'fileutils'
+
 def cloneRepos
 	while (gets)
 		process_line $_
 	end
+end
+
+def force_empty_folder folder
+	if File.directory?(folder)
+		FileUtils.rm_rf(folder)
+	end
+	Dir.mkdir(folder)
 end
 
 def process_line line
@@ -12,20 +21,34 @@ def process_line line
 	
 	begin
 		humanReadableName = humanReadableName.to_s
+		type = type.to_s
 		name = name.gsub(/[^A-Za-z]/, "")
 		repo = repo.gsub(/\r/,"").gsub(/\n/, "").split(/ /)[0]
 		repoName = repo.slice(/.*\/(.+)/, 1)
+		$stderr.puts "Processing #{name}: #{repo}"
+		
 		folder_name = "/tmp/repos/#{name}"
-
-		$stderr.puts "Processing #{repo}"
-
-		if File.directory?("#{folder_name}")
-			Dir.rmdir(folder_name)
+		force_empty_folder(folder_name)
+		
+		if type.include? "Git"
+			$stderr.puts "Begin cloning..."
+			%x(git clone --bare #{repo} #{folder_name})
+		elsif type.include? "Subversion"
+			$stderr.puts "Begin checkout..."
+			tmpDir = "/tmp/svn/#{name}"
+			force_empty_folder(tmpDir)
+			%x(svn co #{repo} #{tmpDir})
+			datestamp = Time.now.strftime("%Y-%m-%d")
+			%x(cd #{tmpDir} && \
+				git init . && \
+				git add . && \
+				git commit -m "snapshot on #{datestamp}" && \
+				git tag -a "head" -m "head" && \
+				git gc --aggressive && \
+				mv .git #{folder_name})
+			FileUtils.rm_rf(tmpDir)
 		end
-		Dir.mkdir("#{folder_name}")
-
-		$stderr.puts "Begin cloning..."
-		%x(git clone --bare #{repo} #{folder_name})
+		$stderr.puts "Finished #{name}"
 	rescue Exception => e
 		$stderr.puts e.message
 		$stderr.puts e.backtrace.inspect
