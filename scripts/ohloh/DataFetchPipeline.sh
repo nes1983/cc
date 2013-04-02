b5efa10efd5bb8e94fef007a69e59426962037a5
Simon Vogt <vsiandroid@gmail.com>
2013-04-02 20:11:38 +0200

    Renamed GitCloner.rb to RepoCloner.rb
    adjusted DataFetchPipeline.sh accordingly

--- scripts/ohloh/DataFetchPipeline.sh
+++ scripts/ohloh/DataFetchPipeline.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 | tee /tmp/logOhlohJavaRepoFetcher \
 | ./FilterRepositories.rb \
 | tee /tmp/logFilterRepositories \
-| parallel --pipe ./GitCloner.rb 2> >(tee -a stderr.log >&2)
+| parallel --pipe ./RepoCloner.rb 2> >(tee -a stderr.log >&2)
