9012365374e6655e0e37be1ce804141e01d658f0
Simon Vogt <vsiandroid@gmail.com>
2013-04-13 18:06:21 +0200

    Fix protoc call
    added absolute path to executable protoc

--- run-protoc.sh
+++ run-protoc.sh
@@ -3,4 +3,4 @@
 # Run from the root of the source tree.
 # Requires the protocol buffer compiler (protoc) to be installed.
 
-find src -name '*.proto' -type f | xargs protoc --java_out=src-gen
+find src -name '*.proto' -type f | xargs /usr/local/bin/protoc --java_out=src-gen
