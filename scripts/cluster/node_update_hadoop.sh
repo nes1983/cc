1e90076d0b04dc5ff5b98c58912c9b5eb69f5c33
Simon Vogt <vsiandroid@gmail.com>
2013-04-13 17:30:13 +0200

    Node update script now supports MRv1 environment

--- scripts/cluster/node_update_hadoop.sh
+++ scripts/cluster/node_update_hadoop.sh
@@ -7,5 +7,10 @@
 cp -v /tmp/core-site.xml /etc/hadoop/conf/
 cp -v /tmp/hadoop-env.sh /etc/hadoop/conf/
 
-service hadoop-yarn-nodemanager restart
-service hadoop-yarn-resourcemanager restart
+#MRv1
+service hadoop-0.20-mapreduce-tasktracker restart
+service hadoop-0.20-mapreduce-jobtracker restart
+
+#MRv2
+#service hadoop-yarn-nodemanager restart
+#service hadoop-yarn-resourcemanager restart
