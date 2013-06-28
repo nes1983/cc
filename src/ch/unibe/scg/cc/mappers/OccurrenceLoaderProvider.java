5e9aba1c7c6d1d113af18e33c2b4a8b1faa436ad
Simon Vogt <vsiandroid@gmail.com>
2013-06-28 11:22:10 +0200

    Fix: OccurrenceLoader
    lookups now over index family
    plus some formatting

--- src/ch/unibe/scg/cc/mappers/OccurrenceLoaderProvider.java
+++ src/ch/unibe/scg/cc/mappers/OccurrenceLoaderProvider.java
@@ -3,13 +3,14 @@ package ch.unibe.scg.cc.mappers;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Collection;
+import java.util.Map.Entry;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import ch.unibe.scg.cc.Protos.Occurrence;
@@ -37,16 +38,18 @@ class OccurrenceLoaderProvider implements Provider<LoadingCache<ByteBuffer, Iter
 	static class File2FunctionFactory implements OccurrenceFactory {
 		@Override
 		public Occurrence make(Result result, ByteBuffer hash) {
-			return Occurrence.newBuilder().setFunctionHash(ByteString.copyFrom(hash))
-					.setFileHash(ByteString.copyFrom(result.getRow())).setFunctionBaseline(Bytes.toInt(result.value()))
-					.build();
+			return Occurrence.newBuilder()
+					.setFunctionHash(ByteString.copyFrom(hash))
+					.setFileHash(ByteString.copyFrom(result.getRow()))
+					.setFunctionBaseline(Bytes.toInt(result.value())).build();
 		}
 	}
 
 	static class Version2FileFactory implements OccurrenceFactory {
 		@Override
 		public Occurrence make(Result result, ByteBuffer hash) {
-			return Occurrence.newBuilder().setVersionHash(ByteString.copyFrom(result.getRow()))
+			return Occurrence.newBuilder()
+					.setVersionHash(ByteString.copyFrom(result.getRow()))
 					.setFileNameHash(ByteString.copyFrom(result.value())).build();
 		}
 	}
@@ -54,7 +57,8 @@ class OccurrenceLoaderProvider implements Provider<LoadingCache<ByteBuffer, Iter
 	static class Project2VersionFactory implements OccurrenceFactory {
 		@Override
 		public Occurrence make(Result result, ByteBuffer hash) {
-			return Occurrence.newBuilder().setProjectNameHash(ByteString.copyFrom(result.getRow()))
+			return Occurrence.newBuilder()
+					.setProjectNameHash(ByteString.copyFrom(result.getRow()))
 					.setTag(Bytes.toString(result.value())).build();
 		}
 	}
@@ -66,13 +70,13 @@ class OccurrenceLoaderProvider implements Provider<LoadingCache<ByteBuffer, Iter
 					@Override
 					public Iterable<Occurrence> load(ByteBuffer hash) throws IOException {
 						Collection<Occurrence> ret = Lists.newArrayList();
-						Scan scan = new Scan(); // Don't use ScanProvider as it's optimized for MR jobs.
-						scan.setCacheBlocks(true); // Other reducers might request the same, so cache.
-						scan.setCaching(1); // We probably don't need adjacent rows. Only get the current.
-						scan.addColumn(Constants.FAMILY, Bytes.getBytes(hash));
-
-						for (Result result : table.getScanner(scan)) {
-							ret.add(occurrenceFactory.make(result, hash));
+						Get indexLookup = new Get(Bytes.getBytes(hash));
+						indexLookup.addFamily(Constants.INDEX_FAMILY);
+						for(Entry<byte[], byte[]> column : 
+								table.get(indexLookup).getFamilyMap(Constants.INDEX_FAMILY).entrySet()) {
+							Get get = new Get(column.getKey());
+							get.addFamily(Constants.FAMILY);
+							ret.add(occurrenceFactory.make(table.get(get), hash));
 						}
 						return ret;
 					}
