f5359f586ca9dccbf3d6e80f702a3acbb7b6bab0
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-12 23:55:50 +0200

    Remove silly constants from ConfigurationProvider.

--- src/ch/unibe/scg/cc/activerecord/ConfigurationProvider.java
+++ src/ch/unibe/scg/cc/activerecord/ConfigurationProvider.java
@@ -6,16 +6,12 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 
 public class ConfigurationProvider implements Provider<Configuration> {
-	public static final String HBASE_CONFIGURATION_HBASE_MASTER = "hbase.master";
-	public static final String HBASE_CONFIGURATION_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
-	public static final String HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT = "hbase.zookeeper.property.clientPort";
-
 	@Override
 	public Configuration get() {
 		Configuration hConf = HBaseConfiguration.create();
-		hConf.set(HBASE_CONFIGURATION_HBASE_MASTER, "leela.unibe.ch:60000");
-		hConf.set(HBASE_CONFIGURATION_ZOOKEEPER_QUORUM, "leela.unibe.ch");
-		hConf.setInt(HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT, 2181);
+		hConf.set("hbase.master", "leela.unibe.ch:60000");
+		hConf.set("hbase.zookeeper.quorum", "leela.unibe.ch");
+		hConf.setInt("hbase.zookeeper.property.clientPort", 2181);
 		hConf.setBoolean("fs.automatic.close", false);
 		return hConf;
 	}
