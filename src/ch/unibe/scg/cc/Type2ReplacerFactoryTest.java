d35decd27bf8219a34f0f419581b6a07ed2363ea
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-13 10:54:03 +0200

    Inline tiny sample into unit test.

--- src/ch/unibe/scg/cc/Type2ReplacerFactoryTest.java
+++ src/ch/unibe/scg/cc/Type2ReplacerFactoryTest.java
@@ -10,19 +10,15 @@ import ch.unibe.scg.cc.regex.Replace;
 
 @SuppressWarnings("javadoc")
 public final class Type2ReplacerFactoryTest {
-	private String sampleString() {
-		return "\t\tfish.stink.Rod.doIt(new int[] { 1, 2 ,3 });\n" + "	}\n";
-	}
-
 	@Test
 	public void testFactory() {
-		final Type2ReplacerFactory factory = new Type2ReplacerFactory();
-		final Replace[] replaces = factory.get();
+		Type2ReplacerFactory factory = new Type2ReplacerFactory();
+		Replace[] replaces = factory.get();
 		assertThat(replaces, is(arrayWithSize(4)));
 
 		Normalizer phase = new Normalizer(replaces);
 
-		final StringBuilder sb = new StringBuilder(sampleString());
+		StringBuilder sb = new StringBuilder("\t\tfish.stink.Rod.doIt(new int[] { 1, 2 ,3 });\n" + "	}\n");
 		phase.normalize(sb);
 		assertThat(sb.toString(), is("\t\tt. t. t. t(t t[] { 1, 1 ,1 });\n\t}\n"));
 	}
