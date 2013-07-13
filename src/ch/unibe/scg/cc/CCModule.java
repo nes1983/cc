d1c9f11f530ecc9548f72021cc3c61171cdbf890
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-13 15:57:08 +0200

    Move Hadoop-specific bindings to MapperModule from CC-Module.

--- src/ch/unibe/scg/cc/CCModule.java
+++ src/ch/unibe/scg/cc/CCModule.java
@@ -3,25 +3,17 @@ package ch.unibe.scg.cc;
 import java.security.MessageDigest;
 import java.util.Comparator;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.util.Bytes;
-
 import ch.unibe.scg.cc.Protos.Clone;
-import ch.unibe.scg.cc.activerecord.ConfigurationProvider;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
-import com.google.inject.name.Names;
 
 //@formatter:off
 public class CCModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(MessageDigest.class).toProvider(MessageDigestProvider.class);
-		bind(Configuration.class).toProvider(ConfigurationProvider.class);
-		bind(new TypeLiteral<Comparator<byte[]>>() {}).toInstance(Bytes.BYTES_COMPARATOR);
-		bind(new TypeLiteral<Comparator<Clone>>() {}).to(CloneComparator.class);
 
-		bind(Boolean.class).annotatedWith(Names.named("writeToWalEnabled")).toInstance(Boolean.FALSE);
+		bind(new TypeLiteral<Comparator<Clone>>() {}).to(CloneComparator.class);
 	}
 }
