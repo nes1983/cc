46e1469b7dfd26866490bbe484abd26d196b3031
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-13 13:40:47 +0200

    Test output to snippets2function.

--- src/ch/unibe/scg/cc/GitWalkerTest.java
+++ src/ch/unibe/scg/cc/GitWalkerTest.java
@@ -2,6 +2,7 @@ package ch.unibe.scg.cc;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.zip.ZipEntry;
@@ -10,6 +11,7 @@ import java.util.zip.ZipInputStream;
 import org.junit.Assert;
 import org.junit.Test;
 
+import ch.unibe.scg.cc.Annotations.Function2Snippets;
 import ch.unibe.scg.cc.Protos.CodeFile;
 import ch.unibe.scg.cc.Protos.Function;
 import ch.unibe.scg.cc.Protos.Project;
@@ -22,6 +24,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
+import com.google.protobuf.ByteString;
 
 @SuppressWarnings("javadoc")
 public final class GitWalkerTest {
@@ -63,7 +66,8 @@ public final class GitWalkerTest {
 			gitWalker.walk(packedRefs, packFile, "Captain Hook");
 		}
 
-		Iterable<Iterable<Cell<Project>>> projectPartitions = i.getInstance(Key.get(new TypeLiteral<CellSource<Project>>() {})).partitions();
+		Iterable<Iterable<Cell<Project>>> projectPartitions = i.getInstance(
+				Key.get(new TypeLiteral<CellSource<Project>>() {})).partitions();
 		assertThat(Iterables.size(projectPartitions), is(1));
 
 		Iterable<Cell<Project>> projects = Iterables.getOnlyElement(projectPartitions);
@@ -73,7 +77,8 @@ public final class GitWalkerTest {
 		Project p = cellCodec.decodeProject(Iterables.getOnlyElement(projects));
 		assertThat(p.getName(), is("Captain Hook"));
 
-		Iterable<Iterable<Cell<Version>>> versionPartitions = i.getInstance(Key.get(new TypeLiteral<CellSource<Version>>() {})).partitions();
+		Iterable<Iterable<Cell<Version>>> versionPartitions = i.getInstance(
+				Key.get(new TypeLiteral<CellSource<Version>>() {})).partitions();
 		assertThat(Iterables.size(versionPartitions), is(1));
 
 		Iterable<Cell<Version>> versions = Iterables.getOnlyElement(versionPartitions);
@@ -82,7 +87,8 @@ public final class GitWalkerTest {
 		assertThat(v.getName(), is("master"));
 		assertThat(v.getProject(), is(p.getHash()));
 
-		Iterable<Iterable<Cell<CodeFile>>> filePartitions = i.getInstance(Key.get(new TypeLiteral<CellSource<CodeFile>>() {})).partitions();
+		Iterable<Iterable<Cell<CodeFile>>> filePartitions = i.getInstance(
+				Key.get(new TypeLiteral<CellSource<CodeFile>>() {})).partitions();
 		assertThat(Iterables.size(filePartitions), is(1));
 
 		Iterable<Cell<CodeFile>> files = Iterables.getOnlyElement(filePartitions);
@@ -93,7 +99,8 @@ public final class GitWalkerTest {
 
 		assertThat(cf.getContents().indexOf("package ch.unibe.scg.cc.mappers;"), is(0));
 
-		Iterable<Iterable<Cell<Function>>> functionPartitions = i.getInstance(Key.get(new TypeLiteral<CellSource<Function>>() {})).partitions();
+		Iterable<Iterable<Cell<Function>>> functionPartitions = i.getInstance(
+				Key.get(new TypeLiteral<CellSource<Function>>() {})).partitions();
 		assertThat(Iterables.size(functionPartitions), is(1));
 
 		Iterable<Cell<Function>> functions = Iterables.getOnlyElement(functionPartitions);
@@ -104,7 +111,8 @@ public final class GitWalkerTest {
 		assertThat(fn.getBaseLine(), is(10));
 		assertThat(fn.getContents().indexOf("public void testProjname"), is(1));
 
-		Iterable<Iterable<Cell<Snippet>>> snippetPartitions = i.getInstance(Key.get(new TypeLiteral<CellSource<Snippet>>() {})).partitions();
+		Iterable<Iterable<Cell<Snippet>>> snippetPartitions = i.getInstance(
+				Key.get(new TypeLiteral<CellSource<Snippet>>() {})).partitions();
 		assertThat(Iterables.size(snippetPartitions), is(1));
 
 		Iterable<Cell<Snippet>> snippets = Iterables.getOnlyElement(snippetPartitions);
@@ -114,8 +122,23 @@ public final class GitWalkerTest {
 		Snippet s7 = cellCodec.decodeSnippet(Iterables.get(snippets, 7));
 
 		assertThat(s0.getFunction(), is(fn.getHash()));
+		assertThat(s0.getLength(), is(5));
 		assertThat(s0.getPosition(), is(0));
 		assertThat(s1.getPosition(), is(1));
+		assertThat(s1.getFunction(), is(fn.getHash()));
 		assertThat(s7.getPosition(), is(7));
+		assertThat(s7.getFunction(), is(fn.getHash()));
+
+		Iterable<Cell<Snippet>> snippets2Funcs = Iterables.getOnlyElement(i.getInstance(
+				Key.get(new TypeLiteral<CellSource<Snippet>>() {}, Function2Snippets.class)).partitions());
+		assertThat(Iterables.size(snippets2Funcs), is(24));
+		assertThat(Iterables.get(snippets2Funcs, 0).rowKey, is(fn.getHash()));
+
+		ByteString snippetHash = Iterables.get(snippets2Funcs, 0).columnKey;
+		boolean found = false;
+		for (Cell<Snippet> cs : snippets) {
+			found |= cellCodec.decodeSnippet(cs).getHash().equals(snippetHash);
+		}
+		assertTrue("Expected the column key of function2snippet to be a snippet hash, but wasn't.", found);
 	}
 }
