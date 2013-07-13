1eb16883afd487e82d39109d5f022686c8817ede
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-13 14:53:42 +0200

    Re-add NormalizerTest.java
    It got lost in the renaming.

---
+++ src/ch/unibe/scg/cc/javaFrontend/NormalizerTest.java
@@ -0,0 +1,28 @@
+package ch.unibe.scg.cc.javaFrontend;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import ch.unibe.scg.cc.Normalizer;
+import ch.unibe.scg.cc.Type2ReplacerFactory;
+
+@SuppressWarnings("javadoc")
+public final class NormalizerTest {
+	@Test
+	public void testNormalizer() {
+		Normalizer n = new Normalizer(new JavaType1ReplacerFactory().get());
+		StringBuilder sb = new StringBuilder("\npublic    static void doIt(char[] arg) {\n");
+		n.normalize(sb);
+		assertThat(sb.toString(), is("\nstatic void doIt(char[] arg) {\n"));
+	}
+
+	@Test
+	public void testNormalize2() {
+		Normalizer n = new Normalizer(new Type2ReplacerFactory().get());
+		StringBuilder sb = new StringBuilder("\npublic    static void doIt(char[] arg) {\n");
+		n.normalize(sb);
+		assertThat(sb.toString(), is("\nt    t t t(t[] t) {\n"));
+	}
+}
