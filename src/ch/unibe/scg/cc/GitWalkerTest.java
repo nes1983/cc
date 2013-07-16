326823ede3d6eca0962ff56532878a12c75f13e3
Simon Vogt <vsiandroid@gmail.com>
2013-07-16 22:13:02 +0200

    GitWalker now needs to be closed

--- src/ch/unibe/scg/cc/GitWalkerTest.java
+++ src/ch/unibe/scg/cc/GitWalkerTest.java
@@ -31,16 +31,17 @@ public final class GitWalkerTest {
 	private static final String TESTREPO = "testrepo.zip";
 
 	@Test
-	public void testProjnameRegex() {
-		GitWalker gitWalker = new GitWalker(null, null);
-		String fullPathString = "har://hdfs-haddock.unibe.ch/projects/testdata.har"
-				+ "/apfel/.git/objects/pack/pack-b017c4f4e226868d8ccf4782b53dd56b5187738f.pack";
-		String projName = gitWalker.extractProjectName(fullPathString);
-		Assert.assertEquals("apfel", projName);
-
-		fullPathString = "har://hdfs-haddock.unibe.ch/projects/dataset.har/dataset/sensei/objects/pack/pack-a33a3daca1573e82c6fbbc95846a47be4690bbe4.pack";
-		projName = gitWalker.extractProjectName(fullPathString);
-		Assert.assertEquals("sensei", projName);
+	public void testProjnameRegex() throws IOException {
+		try(GitWalker gitWalker = new GitWalker(null, null)) {
+			String fullPathString = "har://hdfs-haddock.unibe.ch/projects/testdata.har"
+					+ "/apfel/.git/objects/pack/pack-b017c4f4e226868d8ccf4782b53dd56b5187738f.pack";
+			String projName = gitWalker.extractProjectName(fullPathString);
+			Assert.assertEquals("apfel", projName);
+	
+			fullPathString = "har://hdfs-haddock.unibe.ch/projects/dataset.har/dataset/sensei/objects/pack/pack-a33a3daca1573e82c6fbbc95846a47be4690bbe4.pack";
+			projName = gitWalker.extractProjectName(fullPathString);
+			Assert.assertEquals("sensei", projName);
+		}
 	}
 
 	@Test
