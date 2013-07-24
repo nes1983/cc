54bcff61e2a1f86b7be06a6b8e06937c20583412
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-24 17:12:26 +0200

    Type2 is language independent, so move into CCModule.

--- src/ch/unibe/scg/cc/CCModule.java
+++ src/ch/unibe/scg/cc/CCModule.java
@@ -3,12 +3,15 @@ package ch.unibe.scg.cc;
 import java.security.MessageDigest;
 
 import ch.unibe.scg.cc.Annotations.Function2RoughClones;
-import ch.unibe.scg.cc.Annotations.Snippet2Functions;
 import ch.unibe.scg.cc.Annotations.PopularSnippets;
+import ch.unibe.scg.cc.Annotations.Snippet2Functions;
+import ch.unibe.scg.cc.Annotations.Type2;
 import ch.unibe.scg.cc.Protos.Clone;
 import ch.unibe.scg.cc.Protos.Snippet;
+import ch.unibe.scg.cc.regex.Replace;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.PrivateModule;
 import com.google.inject.TypeLiteral;
 
 //@formatter:off
@@ -23,5 +26,18 @@ public class CCModule extends AbstractModule {
 				.to(PopularSnippetsCodec.class);
 		bind(new TypeLiteral<Codec<Snippet>>() {}).annotatedWith(Snippet2Functions.class)
 				.to(Snippet2FunctionsCodec.class);
+
+		install(new Type2Module());
+	}
+
+	private static class Type2Module extends PrivateModule {
+		@Override
+		protected void configure() {
+			bind(PhaseFrontend.class).annotatedWith(Type2.class).to(Normalizer.class);
+			expose(PhaseFrontend.class).annotatedWith(Type2.class);
+
+			// Private:
+			bind(new TypeLiteral<Replace[]>() {}).toProvider(Type2ReplacerFactory.class);
+		}
 	}
 }
--- src/ch/unibe/scg/cc/javaFrontend/JavaModule.java
+++ src/ch/unibe/scg/cc/javaFrontend/JavaModule.java
@@ -2,12 +2,10 @@ package ch.unibe.scg.cc.javaFrontend;
 
 import ch.unibe.scg.cc.Annotations.Java;
 import ch.unibe.scg.cc.Annotations.Type1;
-import ch.unibe.scg.cc.Annotations.Type2;
 import ch.unibe.scg.cc.Normalizer;
 import ch.unibe.scg.cc.PhaseFrontend;
 import ch.unibe.scg.cc.Populator;
 import ch.unibe.scg.cc.Tokenizer;
-import ch.unibe.scg.cc.Type2ReplacerFactory;
 import ch.unibe.scg.cc.regex.Replace;
 
 import com.google.inject.AbstractModule;
@@ -19,7 +17,6 @@ public class JavaModule extends AbstractModule {
 	protected void configure() {
 		bind(Tokenizer.class).to(JavaTokenizer.class);
 		install(new Type1Module());
-		install(new Type2Module());
 		bind(Populator.class).annotatedWith(Java.class).to(Populator.class); // TODO: Make private																			// Make
 	}
 }
@@ -32,16 +29,5 @@ class Type1Module extends PrivateModule {
 
 		// Private:
 		bind(new TypeLiteral<Replace[]>() {}).toProvider(JavaType1ReplacerFactory.class);
-	}
-}
-
-class Type2Module extends PrivateModule {
-	@Override
-	protected void configure() {
-		bind(PhaseFrontend.class).annotatedWith(Type2.class).to(Normalizer.class);
-		expose(PhaseFrontend.class).annotatedWith(Type2.class);
-
-		// Private:
-		bind(new TypeLiteral<Replace[]>() {}).toProvider(Type2ReplacerFactory.class);
 	}
 }
