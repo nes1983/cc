75f6428d9703d755720c56a4a68ed460ebd298a5
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-24 17:25:32 +0200

    Main no longer needed.

--- src/ch/unibe/scg/cc/Main.java
+++
@@ -1,8 +0,0 @@
-package ch.unibe.scg.cc;
-
-
-public class Main {
-	public static final byte TYPE_1_CLONE = 0x01;
-	public static final byte TYPE_2_CLONE = 0x02;
-	public static final byte TYPE_3_CLONE = 0x03;
-}
