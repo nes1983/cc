3fa8d0b5b42b41c3e7a50c5b7318a4a0f4043a36
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-24 16:59:53 +0200

    Simplify ReplacerProvider.

--- src/ch/unibe/scg/cc/ReplacerProvider.java
+++ src/ch/unibe/scg/cc/ReplacerProvider.java
@@ -1,6 +1,5 @@
 package ch.unibe.scg.cc;
 
-import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -12,15 +11,22 @@ import ch.unibe.scg.cc.regex.Replace;
 
 import com.google.inject.Provider;
 
+/** Extend this class and implement parameterless arguments starting with "make" to add more replaces */
 public abstract class ReplacerProvider implements Provider<Replace[]> {
 	final private Replace[] type = new Replace[] {};
-	final private MethodComparator methodComparator = new MethodComparator();
 
 	@Override
-	public Replace[] get() {
+	public final Replace[] get() {
 		List<Replace> ret = new ArrayList<>();
 		Method[] methods = this.getClass().getMethods();
-		Arrays.sort(methods, methodComparator);
+
+		// Making the output deterministic helps testing.
+		Arrays.sort(methods, new Comparator<Method>() {
+			@Override public int compare(Method m1, Method m2) {
+				return m1.getName().compareTo(m2.getName());
+			}
+		});
+
 		assert methods.length > 0;
 		for (Method method : methods) {
 			if (!method.getName().startsWith("make")) {
@@ -32,23 +38,13 @@ public abstract class ReplacerProvider implements Provider<Replace[]> {
 		return ret.toArray(type);
 	}
 
-	Replace makeReplace(Method method) {
+	final Replace makeReplace(Method method) {
 		try {
 			return (Replace) method.invoke(this);
 		} catch (IllegalArgumentException e) {
 			throw new RuntimeException("Methods named 'make' should not accept arguments.", e);
 		} catch (IllegalAccessException | InvocationTargetException e) {
 			throw new RuntimeException(e);
-		}
-	}
-
-	/** lexicographic comparison of method names */
-	static class MethodComparator implements Comparator<Method>, Serializable {
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public int compare(Method m1, Method m2) {
-			return m1.getName().compareTo(m2.getName());
 		}
 	}
 }
