74cb5437c3b3f5d6eb2ea9596a5690583f61f872
Niko Schwarz <niko.schwarz@gmail.com>
2013-07-25 20:37:25 +0200

    Forgot to add: GitRepoCodec.

---
+++ src/ch/unibe/scg/cc/GitRepoCodec.java
@@ -0,0 +1,19 @@
+package ch.unibe.scg.cc;
+
+import java.io.IOException;
+
+import ch.unibe.scg.cc.Protos.GitRepo;
+
+import com.google.protobuf.ByteString;
+
+class GitRepoCodec implements Codec<GitRepo> {
+	@Override
+	public Cell<GitRepo> encode(GitRepo s) {
+		return new Cell<>(ByteString.copyFromUtf8(s.getProjectName()), ByteString.EMPTY, s.toByteString());
+	}
+
+	@Override
+	public GitRepo decode(Cell<GitRepo> encoded) throws IOException {
+		return GitRepo.parseFrom(encoded.cellContents);
+	}
+}
