807eb52c2ff430106df7ca017e3b9dcda8f47d40
Simon Vogt <vsiandroid@gmail.com>
2013-08-02 13:11:14 +0200

    Sink is closeable, therefore deleted the comment

--- src/org/unibe/scg/cells/Sink.java
+++ src/org/unibe/scg/cells/Sink.java
@@ -3,7 +3,6 @@ package org.unibe.scg.cells;
 import java.io.Closeable;
 
 /** The output of a mapper is written into a sink. Closing closes the underlying CellSink. */
-// TODO: Should this be closeable?
 public interface Sink<T> extends Closeable {
 	/** Write {@code object} into the sink. */
 	void write(T object);
