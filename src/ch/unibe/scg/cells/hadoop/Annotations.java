085a36b490bcc21e20696006cc51508afbf8e8a5
Niko Schwarz <niko.schwarz@gmail.com>
2013-08-05 16:15:35 +0200

    Document hadoop annotations.

--- src/ch/unibe/scg/cells/hadoop/Annotations.java
+++ src/ch/unibe/scg/cells/hadoop/Annotations.java
@@ -10,7 +10,9 @@ import java.lang.annotation.Target;
 
 import javax.inject.Qualifier;
 
+/** Public to allow users to override our configuration. */
 public class Annotations {
+	/** See http://hbase.apache.org/book/perf.writing.html */
 	@Qualifier
 	@Target({ FIELD, PARAMETER, METHOD })
 	@Retention(RUNTIME)
