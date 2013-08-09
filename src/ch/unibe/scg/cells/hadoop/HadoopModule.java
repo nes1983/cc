72e6ebbd8183c161b2c625ba5fd6b34031e5ea4c
Simon Vogt <vsiandroid@gmail.com>
2013-08-09 13:34:25 +0200

    Let HadoopModule implement StorageModule
    bound HBaseCellSource accordingly.

--- src/ch/unibe/scg/cells/hadoop/HadoopModule.java
+++ src/ch/unibe/scg/cells/hadoop/HadoopModule.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.hbase.client.HTable;
 
 import ch.unibe.scg.cells.CellLookupTable;
 import ch.unibe.scg.cells.CellSink;
+import ch.unibe.scg.cells.CellSource;
+import ch.unibe.scg.cells.StorageModule;
 import ch.unibe.scg.cells.hadoop.Annotations.IndexFamily;
 import ch.unibe.scg.cells.hadoop.Annotations.WriteToWalEnabled;
 
@@ -12,10 +14,11 @@ import com.google.inject.TypeLiteral;
 import com.google.protobuf.ByteString;
 
 /** Bindings that expose the cell interfaces */
-public class HadoopModule extends AbstractModule {
+public class HadoopModule extends AbstractModule implements StorageModule {
 	@Override
 	protected void configure() {
 		bind(new TypeLiteral<CellSink<Void>>() {}).to(new TypeLiteral<HBaseCellSink<Void>>() {});
+		bind(new TypeLiteral<CellSource<Void>>() {}).to(new TypeLiteral<HBaseCellSource<Void>>() {});
 		bind(new TypeLiteral<CellLookupTable<Void>>() {}).to(new TypeLiteral<HBaseCellLookupTable<Void>>() {});
 		bindConstant().annotatedWith(WriteToWalEnabled.class).to(false);
 		bind(HTable.class).toProvider(HTableProvider.class);
