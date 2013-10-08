011573c3ba88a90741583da53d8dedf8cef4f710
Niko Schwarz <niko.schwarz@gmail.com>
2013-10-08 13:55:46 +0200

    Rename return value to ret.

--- src/ch/unibe/scg/cells/hadoop/UnibeModule.java
+++ src/ch/unibe/scg/cells/hadoop/UnibeModule.java
@@ -11,12 +11,12 @@ class UnibeModule extends AbstractModule {
 	static class UnibeConfigurationProvider implements Provider<Configuration> {
 		@Override
 		public Configuration get() {
-			Configuration hConf = HBaseConfiguration.create();
-			hConf.set("hbase.master", "leela.unibe.ch:60000");
-			hConf.set("hbase.zookeeper.quorum", "leela.unibe.ch");
-			hConf.setInt("hbase.zookeeper.property.clientPort", 2181);
-			hConf.setBoolean("fs.automatic.close", false);
-			return hConf;
+			Configuration ret = HBaseConfiguration.create();
+			ret.set("hbase.master", "leela.unibe.ch:60000");
+			ret.set("hbase.zookeeper.quorum", "leela.unibe.ch");
+			ret.setInt("hbase.zookeeper.property.clientPort", 2181);
+			ret.setBoolean("fs.automatic.close", false);
+			return ret;
 		}
 	}
 
