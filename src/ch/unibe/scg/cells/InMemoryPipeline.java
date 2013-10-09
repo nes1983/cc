71c2b2b38d44d7db183e68bd623a5e4d8962425d
Niko Schwarz <niko.schwarz@gmail.com>
2013-10-09 20:25:43 +0200

    Have InMemoryPipeline run everything in parallel.
    This works, but really, the client should
    be able to choose between parallel and single-threaded
    for better debugging.
    
    Default should be parallel, though.
    
    The code is veeeeery hack, which I blame on
    the difficulty of passing exceptions between
    threads in Java. This is the best I could come up with.
    I apologize.

--- src/ch/unibe/scg/cells/InMemoryPipeline.java
+++ src/ch/unibe/scg/cells/InMemoryPipeline.java
@@ -2,17 +2,31 @@ package ch.unibe.scg.cells;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.Iterables;
+import com.google.common.io.Closer;
 
-// TODO: Run multithreaded
 /** Implementation of a {@link Pipeline} meant to run in memory. */
 public class InMemoryPipeline<IN, OUT> implements Pipeline<IN, OUT> {
 	final private CellSource<IN> pipeSrc;
 	final private CellSink<OUT> pipeSink;
+
+	/** Incredibly hacky way of moving an exception from one thread to another. */
+	private static class ExceptionHolder {
+		volatile Exception e; // volatile because it can be written and read from different threads.
+	}
 
 	private InMemoryPipeline(CellSource<IN> pipeSrc, CellSink<OUT> pipeSink) { // Don't subclass.
 		this.pipeSrc = pipeSrc;
@@ -82,16 +96,100 @@ public class InMemoryPipeline<IN, OUT> implements Pipeline<IN, OUT> {
 		}
 	}
 
-	/** Run the mapper. Neither source nor sink are closed. However, the mapper does get closed.*/
-	private static <I, E> void run(CellSource<I> src, Codec<I> srcCodec, Mapper<I, E> mapper,
-			CellSink<E> sink, Codec<E> sinkCodec) throws IOException, InterruptedException {
-		try (Mapper<I, E> m = mapper) {
+	/**
+	 * Run the mapper. Neither source nor sink are closed. However, mapper gets closed.
+	 * If an exception occurs in any mapper, it is printed to stderr from within that thread.
+	 * Any one of the exceptions is picked and returned to the caller of this method.
+	 * The threadpool gets shut down on exceptions, too.
+	 */
+	// TODO: Perhaps report all exceptions as suppressed?
+	private static <I, E> void run(CellSource<I> src, Codec<I> srcCodec, final Mapper<I, E> mapper,
+			final CellSink<E> sink, final Codec<E> sinkCodec) throws IOException, InterruptedException {
+
+		try(final Closer mapperCloser = Closer.create()) {
+			mapperCloser.register(mapper);
+
+			// If an exception occurs, write it in here. The indirection is necessary, because inner classes
+			// cannot write to local variables.
+			final ExceptionHolder exceptionHolder = new ExceptionHolder();
+
+			final Object mapperCloserLock = new Object(); // Controls access to mapperCloser.
+
+			// Returns null on IOException, which will be dealt with by the end of the method.
+			final ThreadLocal<Mapper<I, E>> localMapper = new ThreadLocal<Mapper<I, E>>() {
+				@Override protected Mapper<I, E> initialValue() {
+					Mapper<I, E> cloned;
+					try {
+						cloned = cloneMapper(mapper);
+					} catch (IOException e) {
+						exceptionHolder.e = e;
+						System.err.println("Suppressed exception:");
+						e.printStackTrace();
+						return null;
+					}
+					synchronized(mapperCloserLock) {
+						mapperCloser.register(cloned);
+					}
+					return cloned;
+				}
+			};
+
+			ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+			final AtomicInteger waitGroup = new AtomicInteger();
+			// TODO: move decoding into worker thread.
 			for (Iterable<I> decoded : Codecs.decode(src, srcCodec)) {
+				waitGroup.incrementAndGet();
 				Iterator<I> iter = decoded.iterator();
-				I first = iter.next();
-				Iterable<I> row = Iterables.concat(Arrays.asList(first), new AdapterOneShotIterable<>(iter));
-				m.map(first, new AdapterOneShotIterable<>(row), Codecs.encode(sink, sinkCodec));
+				final I first = iter.next();
+				final Iterable<I> row = Iterables.concat(Arrays.asList(first), new AdapterOneShotIterable<>(iter));
+				threadPool.execute(new Runnable() {
+					@Override public void run() {
+						try {
+							@SuppressWarnings("resource") // gets closed by the closer.
+							Mapper<I, E> m = localMapper.get();
+							if (m == null) {
+								return;
+							}
+							m.map(first, new AdapterOneShotIterable<>(row), Codecs.encode(sink, sinkCodec));
+						} catch (Exception e) {
+							exceptionHolder.e = e;
+							System.err.println("Suppressed exception:");
+							e.printStackTrace();
+							return;
+						}
+
+						waitGroup.decrementAndGet();
+					}
+				});
 			}
+			while (true) {
+				if (exceptionHolder.e != null) {
+					Exception cause = exceptionHolder.e; // Don't let anybody overwrite the exception while handling.
+					threadPool.shutdownNow();
+					threadPool.awaitTermination(20, TimeUnit.SECONDS);
+					Throwables.propagateIfPossible(cause, IOException.class, InterruptedException.class);
+					throw new RuntimeException(cause);
+				}
+
+				if (waitGroup.get() == 0) {
+					return;
+				}
+
+				Thread.sleep(70);
+			}
+		}
+	}
+
+	/** Use serialization to clone a mapper. */
+	private static <I, E> Mapper<I, E> cloneMapper(Mapper<I, E> in) throws IOException {
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+		try (ObjectOutputStream out = new ObjectOutputStream(bOut)) {
+			out.writeObject(in);
+		}
+		try {
+			return (Mapper<I, E>) new ObjectInputStream(new ByteArrayInputStream(bOut.toByteArray())).readObject();
+		} catch (ClassNotFoundException e) {
+			throw new AssertionError("We just serialized this object a minute ago. The class cannot be missing", e);
 		}
 	}
 }
