d39251e20b33f9378f84dc0363ed935570f7c4d8
alex <alexey.kolesnichenko@inf.ethz.ch>
2013-10-15 14:47:47 +0200

    Changed counter description

--- src/ch/unibe/scg/cells/Counter.java
+++ src/ch/unibe/scg/cells/Counter.java
@@ -15,7 +15,7 @@ import java.io.Serializable;
  * Note that this counter is unlike a Hadoop counter, in that it isn't only usable from one
  * mapper. This counter is serializable and usable across mappers.
  * However, if the same counter is used from different stages in a pipeline, i.e.
- * from different mappers, the counts may NOT carry over from one stage to another.
+ * from different mappers, the counts MUST NOT carry over from one stage to another.
  */
 public interface Counter extends Serializable {
 	/** Increase the counter by {@code cnt}. */
