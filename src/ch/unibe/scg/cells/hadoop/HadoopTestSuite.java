2500b29ceefa1a63c4f199030b24e122adc3af9e
Niko Schwarz <niko.schwarz@gmail.com>
2013-12-07 15:12:13 +0100

    Adding all cells tests to HadoopTestSuite.

--- src/ch/unibe/scg/cells/hadoop/HadoopTestSuite.java
+++ src/ch/unibe/scg/cells/hadoop/HadoopTestSuite.java
@@ -3,13 +3,24 @@ package ch.unibe.scg.cells.hadoop;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/** A suite with all hadoop-related tests. */
+import ch.unibe.scg.cells.CellsModuleTest;
+import ch.unibe.scg.cells.InMemoryPipelineTest;
+import ch.unibe.scg.cells.InMemoryShufflerTest;
+import ch.unibe.scg.cells.InMemorySourceTest;
+import ch.unibe.scg.cells.LocalCounterTest;
+
+/** A suite with all cells tests. */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
    HadoopPipelineHDFSInputTest.class,
    HadoopPipelineTest.class,
    HadoopCounterTest.class,
-   HBaseCellSinkTest.class
+   HBaseCellSinkTest.class,
+   CellsModuleTest.class,
+   InMemoryPipelineTest.class,
+   InMemoryShufflerTest.class,
+   InMemorySourceTest.class,
+   LocalCounterTest.class,
 })
 public class HadoopTestSuite {
 	// nothing to do.
